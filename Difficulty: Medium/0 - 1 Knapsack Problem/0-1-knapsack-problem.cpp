fe480a1b3ac81653878906541a54a6cbeac7a8b9
Priya Pandey <work.priyafeb2005@gmail.com>
2026-07-31 09:34:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        int n = val.size();
+
+        vector<int> dp(W + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int w = W; w >= wt[i]; w--) {
+                dp[w] = max(dp[w], val[i] + dp[w - wt[i]]);
+            }
+        }
+
+        return dp[W];
+    }
+};
