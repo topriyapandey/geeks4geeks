2a1987c4627a841622020855b89d4e676730121a
Priya Pandey <work.priyafeb2005@gmail.com>
2026-07-31 09:44:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Level Order Traversal/level-order-traversal.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<int> levelOrder(Node *root) {
+        vector<int> ans;
+        
+        if (root == NULL)
+            return ans;
+            
+        queue<Node*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            Node* curr = q.front();
+            q.pop();
+            
+            ans.push_back(curr->data);
+            
+            if (curr->left)
+                q.push(curr->left);
+                
+            if (curr->right)
+                q.push(curr->right);
+        }
+        
+        return ans;
+    }
+};
