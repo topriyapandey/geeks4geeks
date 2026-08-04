bf7c5113d280619c9bd113d537af7d14c51232ac
Priya Pandey <work.priyafeb2005@gmail.com>
2026-08-04 12:37:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    vector<int> rightView(Node *root) {
+        vector<int> ans;
+        
+        if (root == NULL)
+            return ans;
+            
+        queue<Node*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int size = q.size();
+            
+            for (int i = 0; i < size; i++) {
+                Node* curr = q.front();
+                q.pop();
+                
+                if (i == size - 1)
+                    ans.push_back(curr->data);
+                    
+                if (curr->left)
+                    q.push(curr->left);
+                    
+                if (curr->right)
+                    q.push(curr->right);
+            }
+        }
+        
+        return ans;
+    }
+};
