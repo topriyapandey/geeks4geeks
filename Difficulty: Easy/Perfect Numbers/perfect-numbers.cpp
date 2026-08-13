84493bf35329765a7df1dd9122d3af32fe18bb67
Priya Pandey <work.priyafeb2005@gmail.com>
2026-08-13 19:54:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Perfect Numbers/perfect-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    bool isPerfect(int n) {
+        if (n == 1) return false;
+
+        int sum = 1;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+
+                if (i != n / i)
+                    sum += n / i;
+            }
+        }
+
+        return sum == n;
+    }
+};
