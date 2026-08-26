e4430cb518d96736f838d0a42ec37a2d00f79948
Priya Pandey <work.priyafeb2005@gmail.com>
2026-08-26 10:24:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Arithmetic Number/arithmetic-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool inSequence(int a, int b, int c) {
+        if (c == 0)
+            return a == b;
+
+        return (b - a) % c == 0 && (b - a) / c >= 0;
+    }
+};
