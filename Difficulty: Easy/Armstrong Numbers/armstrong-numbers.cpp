95c4771c6c3622fafe5c0261464e46c132d146a2
Priya Pandey <work.priyafeb2005@gmail.com>
2026-08-26 10:21:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool armstrongNumber(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        return sum == original;
+    }
+};
