41ffef8b0774dc775434004e89e2840394948571
Priya Pandey <work.priyafeb2005@gmail.com>
2026-09-01 07:00:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int nthFibonacci(int n) {
+        if (n == 0) return 0;
+
+        int a = 0, b = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+};
