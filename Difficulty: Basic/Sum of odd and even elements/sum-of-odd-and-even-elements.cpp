5cf0dc8c649d645c422de67198fd5152a9e629d5
Priya Pandey <work.priyafeb2005@gmail.com>
2026-09-06 09:52:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of odd and even elements/sum-of-odd-and-even-elements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findSum(int n) {
+        int odd = 0, even = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                even += i;
+            } else {
+                odd += i;
+            }
+        }
+
+        return {odd, even};
+    }
+};
